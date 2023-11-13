a898dcfdf6068706b75e320a246077af9f394ade
Guimart1 <gui.martins.sts@gmail.com>
2023-11-13 01:27:34 -0300

    Att header

---
+++ Confeitaria/Components/header.php
@@ -0,0 +1,29 @@
+<?php
+echo '<nav class="navbar bg-dark border-bottom border-body w-100" data-bs-theme="dark">
+<div class="container-fluid">
+    <a class="navbar-brand" href="#">BOLOS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav" id="navbarHover">
+        <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Produtos</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Sobre nós</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Contato</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link">Avaliações</a>
+        </li>
+        </ul>
+    </div>
+</div>
+</nav>'
+?>
