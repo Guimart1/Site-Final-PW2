ac4a70232022986f2704b215d06b8c91585ca19a
Guimart1 <gui.martins.sts@gmail.com>
2023-12-07 23:26:10 -0300

    Entrega

--- Confeitaria/Components/header-adm.php
+++ Confeitaria/Components/header-adm.php
@@ -7,7 +7,7 @@ $auth = $_SESSION["auth"];
 <header class="p-3 border-bottom bg-secondary text-white shadow" style="height: 10vh">
   <div class="container">
     <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-      <a href="/sites/Confeitaria/Site-Final-PW2/Confeitaria/ClienteArea/index.php" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+      <a href="../../ClienteArea/index.php" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
         <img src="../../ClienteArea/IMG/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
       </a>
       <div class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0 ms-3 fw-bold">
